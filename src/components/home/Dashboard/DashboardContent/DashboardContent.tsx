b403ffb7001e1b94e7bf0728cb7d96c4bd76f3a4
KallNascimento <iksn7@icloud.com>
2022-10-12 22:18:02 -0300

    chat item add

--- src/components/home/Dashboard/DashboardContent/DashboardContent.tsx
+++ src/components/home/Dashboard/DashboardContent/DashboardContent.tsx
@@ -4,34 +4,34 @@ function DashboardContent() {
       <section className="grid gap-10 grid-cols-2 grid-rows-2 justify-items-stretch grid-flow-col md:grid-flow-row sm:grid-flow-row">
         {/* Widget place start */}
         <div className="bg-[#252020] rounded-xl w-[450px] h-[400px]">
-          <div className="flex justify-around">         
-          <div className="py-5">
-            <p className="text-xl text-white font-bold">Task Status:</p>
-          </div>
-          {/* legend text and colors start */}
-          <div>
-            <div className="flex space-x-4 text-white font-bold">
-              <p>New</p>
-              <p>Working</p>
-              <p>Done</p>
+          <div className="flex justify-around">
+            <div className="py-5">
+              <p className="text-xl text-white font-bold">Task Status:</p>
             </div>
-            <div className="flex justify-center space-x-11 py-2 ">
-              <div className="rounded-full bg-[#2544E5] h-5 w-5"></div>
-              <div className="rounded-full bg-[#DDE525] h-5 w-5"></div>
-              <div className="rounded-full bg-[#136A04] h-5 w-5"></div>
+            {/* legend text and colors start */}
+            <div>
+              <div className="flex space-x-4 text-white font-bold">
+                <p>New</p>
+                <p>Working</p>
+                <p>Done</p>
+              </div>
+              <div className="flex justify-center space-x-11 py-2 ">
+                <div className="rounded-full bg-[#2544E5] h-5 w-5"></div>
+                <div className="rounded-full bg-[#DDE525] h-5 w-5"></div>
+                <div className="rounded-full bg-[#136A04] h-5 w-5"></div>
+              </div>
             </div>
-          </div>
-          {/* legends and colors end */}
-          
-          <div className="text-xl py-3 font-bold text-white">
-            <p>
-              My Tasks
-            </p>
-          </div>
+            {/* legends and colors end */}
+
+            <div className="text-xl py-3 font-bold text-white">
+              <p>
+                My Tasks
+              </p>
+            </div>
           </div>
 
           <div id="task-row" className="hover:cursor-pointer">
-                <div className="flex border border-4 border-[#136a04] bg-white rounded-xl w-[450px] py-2 justify-around">
+            <div className="flex border border-4 border-[#136a04] bg-white rounded-xl w-[450px] py-2 justify-around">
               <span className="text-[#C2BBBB] font-bold text-xl self-center">
                 #01
               </span>
@@ -51,8 +51,35 @@ function DashboardContent() {
         </div>
         {/* widget place end */}
 
-        <div>Widget 2</div>
         <div>Widget 3</div>
+
+        {/* Widget Chat place start */}
+        <div className="bg-[#252020] rounded-xl w-[450px] h-[400px]">
+
+          <div className="flex justify-end text-xl py-3 font-bold text-white px-4">
+            Messages
+          </div>
+
+          <div id="user-message-item" className="hover:cursor-pointer">
+            <div className="flex border border-4 border-[#136a04] bg-white rounded-xl w-[450px] py-2">
+              <div id='user-picture'>
+                <img src="https://randomuser.me/api/portraits/women/39.jpg" alt="" className="rounded-full h-20" />
+              </div>
+              <div className="flex flex-col px-4 justify-center">
+                <div className="text-black font-bold">
+                  Kia Malifha
+                </div>
+                <div id='user-message-field'>
+                  Hahaha, it was so funny!
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* widget chat place end */}
+
+
         <div>Widget 4</div>
       </section>
     </div>
