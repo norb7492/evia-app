c6aac748b5f636cafa9f4ffc4420f5d4ccf760cb
KallNascimento <iksn7@icloud.com>
2022-10-13 10:44:24 -0300

    project widget layout

--- src/components/home/Dashboard/DashboardContent/DashboardContent.tsx
+++ src/components/home/Dashboard/DashboardContent/DashboardContent.tsx
@@ -1,3 +1,5 @@
+import { AiOutlineProject } from "react-icons/ai";
+
 function DashboardContent() {
   return (
     <div className='bg-slate-700 h-3/4 m-4 rounded-xl p-6'>
@@ -74,12 +76,38 @@ function DashboardContent() {
               </div>
             </div>
           </div>
-          
+
         </div>
         {/* widget chat place end */}
 
 
-        <div>Widget 4</div>
+        <div className='bg-[#252020] rounded-xl w-[450px] h-[400px]'>
+          <div className='flex justify-end text-xl py-3 font-bold text-white px-4'>
+            Projects
+          </div>
+          <hr />
+          <div id='project-item' className='hover:cursor-pointer text-white text-2xl flex justify-start'>
+            <div className='flex w-auto py-4 px-4'>
+              <div id='project-icon'>
+                <AiOutlineProject />
+              </div>
+              <div className='px-4'>
+                Evia Task Management
+              </div>
+            </div>
+          </div>
+
+          <div id='project-item' className='hover:cursor-pointer text-white text-2xl flex justify-start'>
+            <div className='flex w-auto py-4 px-4'>
+              <div id='project-icon'>
+                <AiOutlineProject />
+              </div>
+              <div className='px-4'>
+                Lidl Dashboard
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
