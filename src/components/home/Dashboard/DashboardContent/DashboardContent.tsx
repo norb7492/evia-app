c3e02d32afc615d36f0b41f3c1716a3b9c1d8009
KallNascimento <iksn7@icloud.com>
2022-10-23 16:16:02 -0300

    add calendar on dashboard

--- src/components/home/Dashboard/DashboardContent/DashboardContent.tsx
+++ src/components/home/Dashboard/DashboardContent/DashboardContent.tsx
@@ -1,3 +1,4 @@
+import Calendar from '../../Calendar/Calendar';
 import MessagesComponent from './MessagesComponent/MessagesComponent';
 import ProjectsComponent from './ProjectsComponent/ProjectsComponent';
 import TasksComponent from './TasksComponnet/TaskComponent';
@@ -7,7 +8,7 @@ function DashboardContent() {
     <div className='bg-slate-700 h-3/4 m-4 rounded-xl p-6'>
       <section className='grid gap-10 grid-cols-2 grid-rows-2 justify-items-stretch grid-flow-col md:grid-flow-row sm:grid-flow-row'>
         <TasksComponent />
-        <div>Widget 3</div>
+        <Calendar/>
         <MessagesComponent />
         <ProjectsComponent />
       </section>
