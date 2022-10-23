287e3f7fbf33f9db347eeba640677061005adaca
KallNascimento <iksn7@icloud.com>
2022-10-23 16:46:30 -0300

    merge components

--- src/components/home/Dashboard/dashboard-content/DashboardContent.tsx
+++ src/components/home/Dashboard/dashboard-content/DashboardContent.tsx
@@ -1,14 +1,16 @@
-import Calendar from '../../Calendar/Calendar';
-import MessagesComponent from './MessagesComponent/MessagesComponent';
-import ProjectsComponent from './ProjectsComponent/ProjectsComponent';
-import TasksComponent from './TasksComponnet/TaskComponent';
+
+import Calendar from '../../calendar/Calendar';
+import MessagesComponent from './messages-component/MessagesComponent';
+import ProjectsComponent from './projects-component/ProjectsComponent';
+import TasksComponent from './tasks-component/TaskComponent';
+
 
 function DashboardContent() {
   return (
     <div className='bg-slate-700 h-3/4 m-4 rounded-xl p-6'>
       <section className='grid gap-10 grid-cols-2 grid-rows-2 justify-items-stretch grid-flow-col md:grid-flow-row sm:grid-flow-row'>
         <TasksComponent />
-        <Calendar/>
+        <Calendar />
         <MessagesComponent />
         <ProjectsComponent />
       </section>
